88ecafe2f5f06f51e8cf744c4750db8793af7661
Li Yi <jayceeyili@gmail.com>
2017-04-06 22:25:16 -0700

    (feat) fired login/signup buttons in Landing page

--- client/components/Landing.jsx
+++ client/components/Landing.jsx
@@ -1,16 +1,35 @@
 import React from 'react';
 
 export default class Layout extends React.Component {
-	constructor(props) {
-		super(props);
+	constructor( props ) {
+		super( props );
+
+    this.state = {
+
+    }
+
+    this.handleLoginClick = this.handleLoginClick.bind( this );
+    this.handleSignupClick = this.handleSignupClick.bind( this );
+  }
+
+  handleLoginClick() {
+    console.log( 'Login!' );
+  }
+
+  handleSignupClick() {
+    console.log( 'Signup!' );
   }
 
   render() {
     return (
       <div>
         <h2>This is the Landing page</h2>
-        <button>Login</button>
-        <button>Signup</button>
+        <button
+          onClick={ this.handleLoginClick }>Login
+        </button>
+        <button
+          onClick={ this.handleSignupClick }>Signup
+        </button>
       </div>
     );
   }
