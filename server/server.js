53b95212659f14c2a0d98c0c4a8bcc46a27014d4
Li Yi <jayceeyili@gmail.com>
2017-04-06 17:16:49 -0700

    {setup} setup basic server file and routes file

--- server/server.js
+++ server/server.js
@@ -1,9 +1,24 @@
+// ««««««««« modules »»»»»»»»»
 const express = require('express');
+const dotenv = require('dotenv').config();
 const bodyParser = require('body-parser');
 const path = require('path');
-const app = express();
 
+// ««««««««« configuration »»»»»»»»»
+// DB config comes here
+
+const app = express();
+const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '../client/public')));
+app.use(bodyParser.json({ entended: true }));
 
-app.listen(3000);
+// ««««««««« routes »»»»»»»»»
+require('./routes')(app);
+
+// ««««««««« start app »»»»»»»»»
+app.listen(port, () => {
+  console.log(`App is listening at port:  ${ port }`);
+});
+
+exports = module.exports = app;
