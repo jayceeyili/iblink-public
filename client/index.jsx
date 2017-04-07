3554038de62c596c4684044d690160f29eff9e73
Li Yi <jayceeyili@gmail.com>
2017-04-06 22:25:16 -0700

    (feat) fired login/signup buttons in Landing page

--- client/index.jsx
+++ client/index.jsx
@@ -1,19 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './components/AppRouter.jsx';
-// import $ from 'jquery';
-
-// class App extends React.Component {
-//   constructor(){
-//     super()
-//   }
-//
-//   render() {
-//     return(
-//       <h1>hello world</h1>
-//     )
-//   }
-// }
 
 ReactDOM.render(
           <AppRouter />,
