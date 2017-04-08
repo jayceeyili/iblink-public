45e8b16aebf90188c0d83a67c3194b29d5033f39
Allen Guan <apguan@gmail.com>
2017-04-08 15:41:45 -0700

    added in root files for actions and reducers

---
+++ client/components/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux';
+import { routerReducer } from 'react-router-redux';
+
+// Import reducers
+
+// pass reducers into combineReducers (in first object)
+const rootReducer = combineReducers({ });
+
+export default rootReducer;
+//Contact GitHub API Training Shop Blog About
