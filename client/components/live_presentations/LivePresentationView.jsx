af8944145abcf125ad6b9e0e87fc9bd2fecf3da7
Allen Guan <apguan@gmail.com>
2017-04-10 12:12:12 -0700

    (feat) added live_presentations directory with components

---
+++ client/components/live_presentations/LivePresentationView.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CarouselView from './CarouselView.jsx';
+
+const LivePresentationView = () => {
+  return (
+    <div>
+      <CarouselView />
+    </div>
+  )
+}
+
+export default LivePresentationView;
