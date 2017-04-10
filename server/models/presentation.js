f7d90c74641409fe97ad024f6a4d5bb1cea53f4f
J-G Demathieu <jdemathieu@gmail.com>
2017-04-10 12:25:00 -0700

    (feat) Add getPresentations()

---
+++ server/models/presentation.js
@@ -0,0 +1,7 @@
+const imagePath = `${__dirname}/../../client/public/user_images/`;
+const mvpPres = [`${imagePath}Slide1.jpg`, `${imagePath}Slide2.jpg`, `${imagePath}Slide3.jpg`];  // For MVP
+
+module.exports.getPresentation = function () {
+  return mvpPres;
+};
+
