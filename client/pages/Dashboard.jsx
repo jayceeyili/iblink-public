c84fdd68ca8a47fb941e5c88010b80a1f04d5793
J-G Demathieu <jdemathieu@gmail.com>
2017-04-10 16:11:52 -0700

    (feat) Starting to develop Dashboard container and stubs for main components

--- client/pages/Dashboard.jsx
+++ client/pages/Dashboard.jsx
@@ -7,9 +7,41 @@ export default class Dashboard extends React.Component {
 
   render() {
     return (
-      <div>
-        <h1>This is the Dashboard page</h1>
+      // { -------PAGE WRAPPER------ }
+      <div className="container-fluid main">
+
+
+        {/* -------LEFT HAND COLUMN------- */}
+        <div className="col-sm-3 account">
+          <Account />
+        </div>
+
+        {/* -------RIGHT HAND COLUMN------- */}
+        <div className="col-sm-9 right9">
+
+          {/* -------TOPBAR------- */}
+          <div className="col-sm-12 topbar">
+            <TopBar event={this.state.currentEvent} owner={this.props.friends[0]} />
+          </div>
+
+          <div className="mainviewWrapper">
+            {/* -------MAIN------- */}
+            <div className="col-sm-8 mainview">
+              {this.state.owner ? (<EventOverview event={this.state.currentEvent} />) : (<FriendEventOverview event={this.state.currentEvent} />)}
+              {/* <FriendEventOverview event={this.state.currentEvent}/>*/}
+
+
+            </div>
+
+            {/* -------CHAT------- */}
+            <div className="col-sm-4 right4">
+              <Chat event={this.state.currentEvent} owner={this.props.friends[0]} />
+            </div>
+          </div>
+
+        </div>
       </div>
+
     );
   }
 }
