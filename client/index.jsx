304696c35cd2f5955d17ae0f5d7498d0697ab7ac
J-G Demathieu <jdemathieu@gmail.com>
2017-04-10 14:48:45 -0700

    (fix) Test bug fix

---
+++ client/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AppRouter from './pages/index.jsx';
+
+ReactDOM.render(
+  <AppRouter />,
+          document.getElementById('app')
+        );
