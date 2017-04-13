296f41ecacd67308e7e145c3b9144fc5c59084ac
J-G Demathieu <jdemathieu@gmail.com>
2017-04-13 14:28:51 -0700

    (refactor) Move dynamic rendering to request handler file

--- server/routes.js
+++ server/routes.js
@@ -6,11 +6,12 @@ module.exports = function (app) {
   // ««««««««« server routes »»»»»»»»»
   let maxSlide = 0;
 
-  app.get('/api', (req, res) => {
-    res.json('Hello World');
-  });
 
-  // other routes come here
+  // ««««««««« User routes »»»»»»»»»
+  app.get('/', renderPage);
+  app.get(/^\d+$/, renderPage);
+
+  // ««««««««« API routes »»»»»»»»»
   app.get('/presentations', (req, res) => {
     res.json(presentation.getPresentation());
   });
@@ -25,3 +26,43 @@ module.exports = function (app) {
     res.json(maxSlide);
   });
 };
+
+
+// ======= Dynamic page rendering ========
+
+function renderPage(req, res) {
+  // Query our mock API asynchronously
+  // const channel = '/\d+/';
+  const localPath = req.params[0];
+  let channel;
+  if (localPath && /^\d+$/.test(localPath)) {
+    channel = localPath;
+  } else {
+    channel = '';
+  }
+
+  // Send the rendered page back to the client
+  res.send(renderFullPage(channel));
+}
+
+function renderFullPage(channel) {
+  return `<!doctype html>
+    <html lang="en">
+      <head>
+        <meta charset="utf-8">
+        <title>iBlink</title>
+        <!--[if lt IE 9]>
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+        <![endif]-->
+        <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <!-- for react-image-gallery... -->
+        <link rel="stylesheet" type="text/css" href="${path.join(__dirname, '../client/public/styles/css/image-gallery.css')}">
+      </head>
+      <body>
+        <div id="app"></div>
+        <script> window.__CHANNEL__ = ${channel} </script>
+        <script src="/static/bundle.js"></script>
+      </body>
+    </html>
+    `;
+}
--- server/server.js
+++ server/server.js
@@ -24,44 +24,7 @@ const compiler = webpack(webpackConfig);
 app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: webpackConfig.output.publicPath }));
 // app.use(webpackHotMiddleware(compiler))
 
-// ««««««««« routes »»»»»»»»»
-app.use(handleRender);
-
-function handleRender(req, res) {
-  // Query our mock API asynchronously
-
-
-  // Compile an initial state
-  const preloadedState = 1;
-
-  // Send the rendered page back to the client
-  res.send(renderFullPage(preloadedState));
-}
-
-function renderFullPage(channel) {
-  return `<!doctype html>
-    <html lang="en">
-      <head>
-        <meta charset="utf-8">
-        <title>iBlink</title>
-        <!--[if lt IE 9]>
-          <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
-        <![endif]-->
-        <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-        <!-- for react-image-gallery... -->
-        <link rel="stylesheet" type="text/css" href="${path.join(__dirname, '../client/public/styles/css/image-gallery.css')}">
-      </head>
-      <body>
-        <div id="app"></div>
-        <script> window.__CHANNEL__ = ${channel} </script>
-        <script src="/static/bundle.js"></script>
-      </body>
-    </html>
-    `;
-}
-
-
-// require('./routes')(app);
+require('./routes')(app);
 
 // ««««««««« start app »»»»»»»»»
 const webServer = app.listen(port, () => {
