e68bdca65a26d770b6c851a39577565d3da30328
Allen Guan <apguan@gmail.com>
2017-04-13 19:43:00 -0700

    (fix) changed sorting functionality

--- server/routes.js
+++ server/routes.js
@@ -17,10 +17,10 @@ router.post('/audience_presentation', controller.audience_presentation.post);
 module.exports = router;
 
 
-app.post('/audience_presentation/add_bookmark', (req, res) => {
-  if (!tempBookmarkStore.includes(req.body.slideIndex)) {
-    bookmarks.push(req.body.slideIndex);
-  }
-  console.log('slide ', slideIndex, ' is being bookmarked');
+  app.post('/audience_presentation/add_bookmark', (req, res) => {
+    if (!tempBookmarkStore.includes(req.body.slideIndex)) {
+      bookmarks.push(req.body.slideIndex);
+    }
+    console.log('slide ', slideIndex, ' is being bookmarked');
 
-})
+  })
