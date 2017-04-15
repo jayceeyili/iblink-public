cdc7399144e45cac85cea2864dad39cce71fca26
J-G Demathieu <jdemathieu@gmail.com>
2017-04-15 11:15:16 -0700

    (demo) Update MVP presentation

--- server/models/presentation.js
+++ server/models/presentation.js
@@ -1,15 +1,27 @@
 const mvpPres = [
   {
-    original: 'http://i.imgur.com/09ztxln.jpg',
-    thumbnail: 'http://i.imgur.com/09ztxln.jpg'
+    original: 'http://i.imgur.com/4m3HhbX.jpg',
+    thumbnail: 'http://i.imgur.com/4m3HhbX.jpg'
   },
   {
-    original: 'http://i.imgur.com/HplUFlH.jpg',
-    thumbnail: 'http://i.imgur.com/HplUFlH.jpg'
+    original: 'http://i.imgur.com/vFBNAvo.jpg',
+    thumbnail: 'http://i.imgur.com/vFBNAvo.jpg'
   },
   {
-    original: 'http://i.imgur.com/8SelpqP.jpg',
-    thumbnail: 'http://i.imgur.com/8SelpqP.jpg'
+    original: 'http://i.imgur.com/HvR3tl5.jpg',
+    thumbnail: 'http://i.imgur.com/HvR3tl5.jpg'
+  },
+  {
+    original: 'http://i.imgur.com/xV46TXK.jpg',
+    thumbnail: 'http://i.imgur.com/xV46TXK.jpg'
+  },
+  {
+    original: 'http://i.imgur.com/ULvmW7i.jpg',
+    thumbnail: 'http://i.imgur.com/ULvmW7i.jpg'
+  },
+  {
+    original: 'http://i.imgur.com/zV6MaXa.jpg',
+    thumbnail: 'http://i.imgur.com/zV6MaXa.jpg'
   }
 ];  // For MVP
 
