b7b673b0516c7db7593da6f21b657138a91aacb9
Allen Guan <apguan@gmail.com>
2017-04-15 13:55:24 -0700

    (refactor) changing how state is dealt with using redux

--- client/components/slide_utilities/toggle_present_button/SendStatusButton.jsx
+++ client/components/slide_utilities/toggle_present_button/SendStatusButton.jsx
@@ -2,38 +2,26 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import Promise from 'bluebird';
-<<<<<<< 1731fdc3e6d689f7cf683d2dfa0b57455bd00249
 import store from '../../../pages/store.js'
-=======
->>>>>>> (fix) fixed bookmarking log
+>>>>>>> (refactor) changing how state is dealt with using redux
 import { SendStatus } from './../../../actions/socketAction';
 
 class SendStatusButton extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      presenterIsOn: true
-    }
+    // this.state = {
+    //   presenterIsOn: true
+    // }
     this.handlePresentButton = this.handlePresentButton.bind(this);
   }
 
   handlePresentButton() {
-<<<<<<< 1731fdc3e6d689f7cf683d2dfa0b57455bd00249
     this.props.SendStatus();
     this.setState({presenterIsOn: !this.state.presenterIsOn}, () => {
       if (!this.state.presenterIsOn) {
         fetch('/audience_presentation/store_bookmark')
         .then(response => response.json())
         .then((bookmarkedSlides) => {
-=======
-    this.setState({presenterIsOn: !this.state.presenterIsOn}, () => {
-      if (!this.state.presenterIsOn) {
-        console.log('presenterIsOn === false');
-        fetch('/audience_presentation/store_bookmark')
-        .then(response => response.json())
-        .then((bookmarkedSlides) => {
-          console.log('GET to /audience_presentation/store_bookmark is receiving response');
->>>>>>> (fix) fixed bookmarking log
           console.log('bookmarked: ', bookmarkedSlides);
         })
         .catch((error) => {
@@ -41,10 +29,21 @@ class SendStatusButton extends React.Component {
         });
       }
     });
-<<<<<<< 1731fdc3e6d689f7cf683d2dfa0b57455bd00249
-=======
-    // this.props.SendStatus();
->>>>>>> (fix) fixed bookmarking log
+
+    // this.setState({presenterIsOn: !this.state.presenterIsOn}, () => {
+    //   if (!this.state.presenterIsOn) {
+    //     console.log('presenterIsOn === false');
+    //     fetch('/audience_presentation/store_bookmark')
+    //     .then(response => response.json())
+    //     .then((bookmarkedSlides) => {
+    //       console.log('GET to /audience_presentation/store_bookmark is receiving response');
+    //       console.log('bookmarked: ', bookmarkedSlides);
+    //     })
+    //     .catch((error) => {
+    //       console.error(error);
+    //     });
+    //   }
+    // });
   }
 
   render() {
