bd46e9ee541a288af068c6e3dd8c1f1907533ffc
Johnny Chen <johnny.chen54@gmail.com>
2017-04-22 11:01:32 -0700

    Complete removing bookmarks from the backend

--- server/routes.js
+++ server/routes.js
@@ -16,11 +16,11 @@ router.get('/audience_presentation', controller.audience_presentation.get);
 router.post('/audience_presentation', controller.audience_presentation.post);
 
 router.post('/audience_presentation/add_bookmark', controller.audience_presentation_add_bookmark.post);
-router.post('/audience_presentation/add_note', controller.audience_presentation_add_note.post);
-
+router.post('/audience_presentation/remove_bookmark', controller.audience_presentation_remove_bookmark.post);
 router.get('/audience_presentation/get_bookmarks', controller.audience_presentation_get_bookmarks.get);
 router.get('/audience_presentation/store_bookmarks', controller.audience_presentation_store_bookmarks.get);
 
-router.post('/presenter_presentation', controller.presenter_presentation.post)
+router.post('/presenter_presentation', controller.presenter_presentation.post);
+router.post('/audience_presentation/add_note', controller.audience_presentation_add_note.post);
 
 module.exports = router;
