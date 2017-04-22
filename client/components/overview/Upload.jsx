4d218b5a1a755c7aaed52505e5883f1f59c16555
J-G Demathieu <jdemathieu@gmail.com>
2017-04-21 19:26:57 -0700

    (feat) Styling of upload button

--- client/components/overview/Upload.jsx
+++ client/components/overview/Upload.jsx
@@ -11,12 +11,30 @@ class Upload extends React.Component {
   handleUpload() {
     console.log('Upload request detected!');
 
-    this.uploadWidget = cloudinary.openUploadWidget({ upload_preset: 'nl29au84', cloud_name: 'iblink' },
+
+    this.uploadWidget = cloudinary.openUploadWidget({
+      upload_preset: 'nl29au84',
+      cloud_name: 'iblink',
+      button_caption: 'Upload slides',
+      theme: 'minimal',
+      sources: ['local'],
+      text:
+      {
+        'sources.local.title': 'Your presentations',
+        'sources.local.drop_files': 'Drop all slides here',
+        'sources.local.drop_or': 'Or',
+        'sources.local.select_files': 'Select all slides',
+        'progress.retry_upload': 'Please try again',
+        'progress.failed_note': 'Some of your slides failed uploading.'
+      }
+    },
       (error, result) => {
         if (error) {
-      	console.error('Error in upload:', error);
+      	  console.error('Error in upload:', error);
         } else {
-      	console.log('Upload successful! Result:', result);
+      	  console.log('Upload successful! Result:', result);
+          // update the state to add this presentation to the user's set
+          // send the presentation to the server
         }
       // this.uploadWidget.close();
       });
