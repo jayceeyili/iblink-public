c76170d03651907f7c1218652c5178f0e0fe02b2
Li Yi <jayceeyili@gmail.com>
2017-04-24 16:45:36 -0700

    add extra attributes to fake data

--- server/models/presentation.js
+++ server/models/presentation.js
@@ -5,26 +5,50 @@ const mvpPres = {
     {
       original: 'http://i.imgur.com/1NDbR9t.jpg',
       thumbnail: 'http://i.imgur.com/1NDbR9t.jpg'
+      id: 14234,
+      bookmark: false,
+      note: '',
+      tweet: false
     },
     {
       original: 'http://i.imgur.com/kTQhA6J.jpg',
       thumbnail: 'http://i.imgur.com/kTQhA6J.jpg'
+      id: 47684,
+      bookmark: false,
+      note: 'I like this slide',
+      tweet: false
     },
     {
       original: 'http://i.imgur.com/jUkgkhB.jpg',
       thumbnail: 'http://i.imgur.com/jUkgkhB.jpg'
+      id: 65847,
+      bookmark: false,
+      note: '',
+      tweet: false
     },
     {
       original: 'http://i.imgur.com/8IzciiM.jpg',
       thumbnail: 'http://i.imgur.com/8IzciiM.jpg'
+      id: 56734,
+      bookmark: false,
+      note: 'I do not like this slide',
+      tweet: false
     },
     {
       original: 'http://i.imgur.com/ULvmW7i.jpg',
       thumbnail: 'http://i.imgur.com/ULvmW7i.jpg'
+      id: 43573,
+      bookmark: false,
+      note: 'just so so',
+      tweet: false
     },
     {
       original: 'http://i.imgur.com/aSgIylL.jpg',
       thumbnail: 'http://i.imgur.com/aSgIylL.jpg'
+      id: 34512,
+      bookmark: false,
+      note: '',
+      tweet: false
     }
   ]
 };
