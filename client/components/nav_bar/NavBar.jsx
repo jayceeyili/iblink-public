df7e257536e607744729777970e49e20e4892c2c
Allen Guan <apguan@gmail.com>
2017-04-24 20:05:04 -0700

    (refactor) moving login to separate page

--- client/components/nav_bar/NavBar.jsx
+++ client/components/nav_bar/NavBar.jsx
@@ -3,6 +3,8 @@ import Upload from '../uploader/Upload.jsx';
 import style from './style.css';
 import Login from '../../containers/loginContainer.js';
 
+
+//Login component used for testing purpose. Permanent solution will be through a seperate page
 class NavBar extends React.Component {
   constructor() {
     super()
