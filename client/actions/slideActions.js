9439877c9cf091701e0b5d00f9699280007ef505
Li Yi <jayceeyili@gmail.com>
2017-04-25 09:31:11 -0700

    (feat) fully implement note function

--- client/actions/slideActions.js
+++ client/actions/slideActions.js
@@ -1,15 +1,83 @@
 import ActionType from './actionType';
 
-export function updateText( text ) {
-  return {
-    type: ActionType.UpdateText,
-    text
-  };
-}
-
+// export function updateText( text ) {
+//   return {
+//     type: ActionType.UpdateText,
+//     text
+//   };
+// }
+//
 export function updateSlideState( currentSlide ) {
   return {
     type: ActionType.UpdateSlideState,
     currentSlide
   }
 };
+
+export function addNote( body ) {
+  fetch('/audience_presentation/add_note', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify( body )
+  })
+  .then( res => {
+    if ( !res.ok ) {
+      throw new Error( 'Failed to Add Note!' );
+    }
+    return res.json()
+  } )
+  .then( ( data ) => console.log( 'message from addNote action: ', data ) )
+  .catch( error => console.error( 'error from addNote action: ', error) );
+
+  return {
+    type: ActionType.AddNote,
+    text: body.note
+  }
+}
+
+export function updateNote( body ) {
+  fetch('/audience_presentation/update_note', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify( body )
+  })
+  .then( res => {
+    if ( !res.ok ) {
+      throw new Error( 'Failed to Update Note!' );
+    }
+    return res.json()
+  } )
+  .then( ( data ) => console.log( 'message from updateNote action: ', data ) )
+  .catch( error => console.error( 'error from updateNote action: ', error) );
+
+  return {
+    type: ActionType.UpdateNote,
+    text: body.note
+  }
+}
+
+export function deleteNote( body ) {
+  fetch('/audience_presentation/delete_note', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify( body )
+  })
+  .then( res => {
+    if ( !res.ok ) {
+      throw new Error( 'Failed to Delete Note!' );
+    }
+    return res.json()
+  } )
+  .then( ( data ) => console.log( 'message from deleteNote action: ', data ) )
+  .catch( error => console.error( 'error from deleteNote action: ', error) );
+
+  return {
+    type: ActionType.DeleteNote,
+  }
+}
