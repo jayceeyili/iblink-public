80f248dce9c82269570b696648daee9889f4c7f1
Johnny Chen <johnny.chen54@gmail.com>
2017-04-27 09:40:05 -0700

    Add dynamic presentation id bookmark db queries

--- server/models/bookmark.js
+++ server/models/bookmark.js
@@ -1,32 +1,56 @@
-const presentationObject = require('./presentation');
+const Promise = require('bluebird');
 const models = require('../../database/models/index');
+const getTargetPresentationSlides = Promise.promisify(require('./slide').getTargetPresentationSlides);
 
-const slides = presentationObject.getPresentation().slides;
-
-module.exports.addBookmark = function (bookmarkedSlideUrl, userId) {
-  for (var i = 0; i < slides.length; i++) {
-    if (slides[i].original === bookmarkedSlideUrl) {
-      models.Bookmark.create({
-        slide_id: slides[i].id,
-        user_id: userId
-      })
-      .catch(err => console.log(err));
-      break;
+module.exports.addBookmark = function (bookmarkedSlideUrl, userId, presentationId) {
+  getTargetPresentationSlides(presentationId)
+  .then((slides) => {
+    for (var i = 0; i < slides.length; i++) {
+      if (slides[i].dataValues.original === bookmarkedSlideUrl) {
+        models.Bookmark.create({
+          slide_id: slides[i].dataValues.id,
+          user_id: userId
+        })
+        .catch(err => console.log(err));
+        break;
+      }
     }
-  }
+  })
+  .catch(err => console.log(err));
 };
 
-module.exports.removeBookmark = function (bookmarkedSlideUrl, userId) {
-  for (var i = 0; i < slides.length; i++) {
-    if (slides[i].original === bookmarkedSlideUrl) {
-      models.Bookmark.destroy({
-        where: {
-          slide_id: slides[i].id,
-          user_id: userId
-        }
-      })
-      .catch(err => console.log(err));
-      break;
+module.exports.removeBookmark = function (bookmarkedSlideUrl, userId, presentationId) {
+  getTargetPresentationSlides(presentationId)
+  .then((slides) => {
+    for (var i = 0; i < slides.length; i++) {
+      if (slides[i].dataValues.original === bookmarkedSlideUrl) {
+        models.Bookmark.destroy({
+          where: {
+            slide_id: slides[i].dataValues.id,
+            user_id: userId
+          }
+        })
+        .catch(err => console.log(err));
+        break;
+      }
     }
-  }
+  })
+  .catch(err => console.log(err));
+};
+
+module.exports.checkIsSlideBookmarked = function (slideId, userId, callback) {
+  let isSlideBookmarked = false;
+  models.Bookmark.findOne({
+    where: {
+      slide_id: slideId,
+      user_id: userId
+    }
+  })
+  .then((bookmarkQueryResult) => {
+    if (bookmarkQueryResult !== null) {
+      isSlideBookmarked = true;
+    }
+    callback(null, isSlideBookmarked);
+  })
+  .catch(err => console.log(err));
 };
