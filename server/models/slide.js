a26533123564728003526fece1039eb2ef6bf7c8
Johnny Chen <johnny.chen54@gmail.com>
2017-04-27 09:50:53 -0700

    Ready to create another branch

---
+++ server/models/slide.js
@@ -0,0 +1,12 @@
+module.exports.getTargetPresentationSlides = function (presentationId, callback) {
+  models.Slide.findAll({
+    where: {
+      presentation_id: presentationId
+    }
+  })
+  .then((targetPresentationSlides) => {
+    // console.log('FROM slideUtil --> targetPresentationSlides: ', targetPresentationSlides);
+    callback(null, targetPresentationSlides);
+  })
+  .catch(err => console.log(err));
+};
